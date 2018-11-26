2d8fd104d156a10fc5d9f98964d58ab86dc6a81d
Edward Timmer <edtimmer@gmail.com>
2018-11-25 21:08:57 -0500

    added user recipes to user profile

--- resolvers.js
+++ resolvers.js
@@ -36,6 +36,11 @@ exports.resolvers = {
       }
     },
 
+    getUserRecipes: async (root, { username }, { Recipe }) => {
+      const userRecipes = await Recipe.find({ username }).sort({ createdDate: 'desc' });
+      return userRecipes;
+    },
+
     getCurrentUser: async (root, args, { currentUser, User }) => {
       if (!currentUser) {
         return null;

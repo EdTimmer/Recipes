01e2fdb5baf7f20a2e5727a3515efa61487c7549
Edward Timmer <edtimmer@gmail.com>
2018-12-01 17:03:06 -0500

    added fragments to queries

--- client/src/components/Recipe/LikeRecipe.js
+++ client/src/components/Recipe/LikeRecipe.js
@@ -34,13 +34,13 @@ class LikeRecipe extends React.Component {
   handleLike = (likeRecipe, unlikeRecipe) => {
     if (this.state.liked) {
       likeRecipe().then(async ({ data }) => {
-        console.log(data);
+        // console.log(data);
         await this.props.refetch();
       })
     }
     else {
       unlikeRecipe().then(async ({ data }) => {
-        console.log(data);
+        // console.log(data);
         await this.props.refetch();
       })
     }
